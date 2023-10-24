130efd91c33969d2c70170e38656649a4f9775cc
Johan Johnsson <johan@johnsson-net.se>
2023-10-24 14:57:35 +0200

    replaced methods with printTransaction

--- src/main/java/org/example/Expense.java
+++ src/main/java/org/example/Expense.java
@@ -13,7 +13,9 @@ public class Expense extends Transaction {
         this.category = category;
     }
 
-    public void printExpense() {
+    // Polymorphism, se Income.java och dokument.
+    @Override
+    public void printTransaction() {
         System.out.printf("|%-15s |%-15s |%-15s |%-15s\n", super.getName(), category, super.getAmount() + " kr", super.getDate());
     }
 
